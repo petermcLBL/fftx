ae55e70e4e2eb0014436f30ba06281e9618cc162
Peter McCorquodale <PWMcCorquodale@lbl.gov>
2026-09-16 18:41:24 -0400

    Load modules instead of setting paths in frontier script.

--- test_scripts/test_batch_frontier.sh
+++ test_scripts/test_batch_frontier.sh
@@ -22,10 +22,9 @@
 #SBATCH --output=test_batch_frontier_out.%J
 #SBATCH --error=test_batch_frontier_err.%J
 
-export FABRIC_PATH=/opt/cray/libfabric/1.22.0
-export ROCM_PATH=/opt/rocm-6.2.4
-export CRAY_MPICH_DIR=/opt/cray/pe/mpich/8.1.31/ofi/gnu/12.3
-export PYTHON_PATH=/opt/cray/pe/python/3.11.7
-export LD_LIBRARY_PATH=$PYTHON_PATH/lib:$FABRIC_PATH/lib64:$CRAY_MPICH_DIR/lib:$ROCM_PATH/lib:$FABRIC_PATH/lib64
+module purge
+module load rocm
+module load PrgEnv-gnu
+module load python
 
 source $FFTX_HOME/test_scripts/test_batch_script.sh
